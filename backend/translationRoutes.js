e79c6938b1d4209d19e4359576007ed20c82717c
Patrick Elfert <patrickelfert5@gmail.com>
2024-09-13 14:07:11 +0200

    add nest js application

--- backend/translationRoutes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-async function translationRoutes(fastify) {
-    fastify.get('/', async (request, reply) => {
-        return { hello: 'world' };
-    });
-}
-exports.default = translationRoutes;
--- backend/translationRoutes.ts
+++
@@ -1,9 +0,0 @@
-import {FastifyInstance} from "fastify";
-
-async function translationRoutes (fastify: FastifyInstance) {
-    fastify.get('/', async (request, reply) => {
-        return { hello: 'world' }
-    })
-}
-
-export default translationRoutes;
