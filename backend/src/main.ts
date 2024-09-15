2b6b1cd9d221a39dcbe2301645c4eb02acc5ee31
Patrick Elfert <patrickelfert5@gmail.com>
2024-09-15 15:47:13 +0200

    fix wrong open api types

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,6 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import {DocumentBuilder, SwaggerModule} from "@nestjs/swagger";
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as fs from 'node:fs';
+import { stringify } from 'yaml';
+import { patchNestjsSwagger } from '@anatine/zod-nestjs';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -8,7 +11,9 @@ async function bootstrap() {
     .setTitle('Magni Miner')
     .setVersion('1.0')
     .build();
+  patchNestjsSwagger();
   const document = SwaggerModule.createDocument(app, config);
+  fs.writeFileSync('./swagger-spec.json', JSON.stringify(document));
   SwaggerModule.setup('api', app, document);
   await app.listen(3000);
 }
