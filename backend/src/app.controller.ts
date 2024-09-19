e2267bc65593ec6fca8665b7aeba89be204867e9
Patrick Elfert <patrickelfert5@gmail.com>
2024-09-19 19:51:33 +0200

    add endpoint for parsing srt files to tokens

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,13 +1,23 @@
-import { Body, Controller, Get, Post, Query, UsePipes } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Query,
+  UploadedFile,
+  UseInterceptors,
+  UsePipes,
+} from '@nestjs/common';
 import { AppService } from './app.service';
 import { Param } from '@nestjs/common';
 import { z } from 'zod';
-import { extendApi } from '@anatine/zod-openapi';
+import { extendApi, extendZodWithOpenApi } from '@anatine/zod-openapi';
 import { createZodDto, ZodValidationPipe } from '@anatine/zod-nestjs';
 import { getJapaneseLanguage, getSubtitles } from './subtitles/fetch-subtitles';
 import { tokenizeSentence } from './subtitles/parse-subtitles';
 import { TokenResult, tokenSchema } from './subtitles/segmentation-schema';
-import { ApiCreatedResponse, ApiResponse } from '@nestjs/swagger';
+import { ApiBody, ApiConsumes, ApiCreatedResponse } from '@nestjs/swagger';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { Express } from 'express';
 
 const TokenizedSubtitleResponseSchema = extendApi(
   z.object({
@@ -31,6 +41,16 @@ const GetTokenizedSubtitleQuerySchema = extendApi(
   }),
 );
 
+const GetTokenizedSubtitleFileSchema = extendApi(
+  z.object({
+    file: extendApi(z.string(), { type: 'string', format: 'binary' }),
+  }),
+);
+
+export class GetTokenizedSubtitleFileDto extends createZodDto(
+  GetTokenizedSubtitleFileSchema,
+) {}
+
 export class GetTokenizedSubtitleQuery extends createZodDto(
   GetTokenizedSubtitleQuerySchema,
 ) {}
@@ -52,8 +72,6 @@ export class AppController {
   async getTokenizedSubtitles(
     @Param('id') id: string,
   ): Promise<TokenizedSubtitleResponseDto[]> {
-    console.log(`Getting subtitles for ${id}`);
-
     const lang = await getJapaneseLanguage(id);
     const subtitles = await getSubtitles(lang);
     const tokenizedSubtitlesResponse: TokenizedSubtitleResponseDto[] = [];
@@ -70,9 +88,37 @@ export class AppController {
         end: Number(subtitle.dur) + Number(subtitle.start),
       });
     }
+    return tokenizedSubtitlesResponse;
+  }
 
-    console.log(`Returning subtitles for ${id}`);
+  @Post('subtitleTokenFromSrt')
+  @UseInterceptors(FileInterceptor('file'))
+  @ApiConsumes('multipart/form-data')
+  @ApiBody({
+    description: 'List of cats',
+    type: GetTokenizedSubtitleFileDto,
+  })
+  async getTokenizedSubtitlesFromSrt(
+    @UploadedFile() file: Express.Multer.File,
+  ): Promise<TokenizedSubtitleResponseDto[]> {
 
+      //Todo: SRT Parsers
+
+    const subtitles = parser.fromSrt(file.buffer.toString('utf-8'));
+    const tokenizedSubtitlesResponse: TokenizedSubtitleResponseDto[] = [];
+
+    const tokenPromises: Promise<TokenResult[]>[] = subtitles.map((s) =>
+      tokenizeSentence(s.text),
+    );
+    const resultTokens = await Promise.all(tokenPromises);
+
+    for (const [index, subtitle] of subtitles.entries()) {
+      tokenizedSubtitlesResponse.push({
+        tokens: resultTokens[index],
+        start: subtitle.startSeconds,
+        end: subtitle.endSeconds,
+      });
+    }
     return tokenizedSubtitlesResponse;
   }
 
@@ -83,11 +129,11 @@ export class AppController {
   async getTokenizedSubtitle(
     @Query() tokenizedSubtitleQuery: GetTokenizedSubtitleQuery,
   ): Promise<TokenizedSubtitleResponseDto> {
-      console.log('Getting tokens')
+    console.log('Getting tokens');
     const tokenizedSubtitle = await tokenizeSentence(
       tokenizedSubtitleQuery.text,
     );
-      console.log('Returning tokens')
+    console.log('Returning tokens');
     return {
       tokens: tokenizedSubtitle,
     };
@@ -100,9 +146,9 @@ export class AppController {
   async getSubtitles(@Param('id') id: string): Promise<SubtitleResponseDto[]> {
     const lang = await getJapaneseLanguage(id);
     return (await getSubtitles(lang)).map((s) => ({
-        text: s.text,
-        start: Number(s.start),
-        end: Number(s.start) + Number(s.dur),
+      text: s.text,
+      start: Number(s.start),
+      end: Number(s.start) + Number(s.dur),
     }));
   }
 }
