cc7f0e84e33005d62c7dadf5f78cd7649637d13b
Patrick Elfert <patrickelfert5@gmail.com>
2024-09-19 19:51:41 +0200

    add player component

--- frontend/tabs/player.tsx
+++ frontend/tabs/player.tsx
@@ -1,7 +1,52 @@
-import VideoPlayer from "~components/video-player";
+import React, { ChangeEvent, useState } from "react"
+
+import "style.css"
 
 function Player() {
-    return <VideoPlayer/>;
+  const [videoSrc, setVideoSrc] = useState<string | null>(null)
+  const [subtitleSrc, setSubtitleSrc] = useState<string | null>(null)
+
+  const fileSelected = (
+    e: ChangeEvent<HTMLInputElement>,
+    onFileSelected: (fileUrl: string) => void
+  ) => {
+    const file = e.target.files?.[0]
+    if (file) {
+      const fileURL = URL.createObjectURL(file)
+      onFileSelected(fileURL)
+    }
+  }
+
+  const handleVideoFileSelected = (e: ChangeEvent<HTMLInputElement>) => {
+    fileSelected(e, (fileUrl) => setVideoSrc(fileUrl))
+  }
+
+  const handleSubtitleFileSelected = (e: ChangeEvent<HTMLInputElement>) => {
+    fileSelected(e, (subtitleSrc) => setSubtitleSrc(subtitleSrc))
+  }
+
+  return (
+      <div className={"flex flex-col"}>
+          <input
+              className={"py-2"}
+              type="file"
+              accept="video/*"
+              onChange={handleVideoFileSelected}
+          />
+          <input
+              className={"py-2"}
+              type="file"
+              accept=".srt"
+              onChange={handleSubtitleFileSelected}
+          />
+          {videoSrc && (
+              <video className={"h-full w-full"} controls>
+                  <source src={videoSrc} type="video/mp4"/>
+                  Your browser does not support the video tag.
+              </video>
+          )}
+      </div>
+  )
 }
 
-export default Player;
+export default Player
